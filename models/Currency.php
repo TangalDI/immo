e4a08cca6330f8cce38e64583219220d77fa17c5
root <root@immo.n3.ru>
2019-10-25 02:13:57 +0300

    Model by gii

---
+++ models/Currency.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "currency".
+ *
+ * @property int $id
+ * @property string $name
+ * @property double $rate
+ * @property string $insert_dt
+ */
+class Currency extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'currency';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'rate', 'insert_dt'], 'required'],
+            [['rate'], 'number'],
+            [['insert_dt'], 'safe'],
+            [['name'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'rate' => 'Rate',
+            'insert_dt' => 'Insert Dt',
+        ];
+    }
+}
